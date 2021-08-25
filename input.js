26f358f93921be6ebb01c479af77812ef5ab4714
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-25 14:04:26 -0400

    Added a send key

--- input.js
+++ input.js
@@ -1,11 +1,34 @@
+let connection = '';
+
 const handleUserInput = (key) => {
-  process.stdout.write(key);
+
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key == 'w') {
+    connection.write('Move: up')
+  }
+
+  if (key == 'a') {
+    connection.write('Move: left')
+  }
+
+  if (key == 's') {
+    connection.write('Move: down')
+  }
+
+  if (key == 'd') {
+    connection.write('Move: right')
+  }
+
+  if (key == 'h') {
+    connection.write('Say: hey!');
+  }
 }
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,4 +38,5 @@ const setupInput = () => {
   return stdin;
 };
 
+setupInput();
 module.exports = {setupInput}
